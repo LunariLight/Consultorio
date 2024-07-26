a1327ee5f221a69f0bb43e62cdbc08442b932906
Chonees <lucasbranchini196@gmail.com>
2024-07-26 16:31:06 -0300

    number en el form de contacto

--- front/src/routes/Contacto.jsx
+++ front/src/routes/Contacto.jsx
@@ -6,15 +6,18 @@ export const Contacto = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    const [number, setNumber] = useState('');
+
   
     const handleSubmit = (e) => {
       e.preventDefault();
       // Aquí podrías manejar el envío del formulario, por ejemplo, enviarlo a un servidor
-      console.log('Formulario enviado:', { name, email, message });
+      console.log('Formulario enviado:', { name, email, message, number });
       // Resetear el formulario
       setName('');
       setEmail('');
       setMessage('');
+      setNumber('');
     };
   
   return (
@@ -36,6 +39,15 @@ export const Contacto = () => {
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Numero:</label>
+          <input
+            type="number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
             required
           />
         </div>
